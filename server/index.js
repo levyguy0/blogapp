70ec586d24b46933a47a483f58ebbb5641b618c6
Guy Levy <103501989+levyguy0@users.noreply.github.com>
2024-04-04 18:36:45 +0100

    create users route

--- server/index.js
+++ server/index.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const users = require("./routes/users");
 
 const app = express();
 const PORT = 8080;
 
 app.use(cors());
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/users", users);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
