c7fd5ac57ce6ef295ae7f8b5ee40a63d562a4601
levyguy0 <103501989+levyguy0@users.noreply.github.com>
2024-04-06 23:27:06 +0100

    create add post

--- server/routes/blogposts.js
+++ server/routes/blogposts.js
@@ -1,12 +1,43 @@
 const express = require("express");
-const { PrismaClient } = require("@prisma/client");
+const { PrismaClient, CategoryName } = require("@prisma/client");
+const auth = require("../middleware/auth");
 
 const router = express.Router();
 const prisma = new PrismaClient();
 
-router.get("/", async (req, res) => {
+router.get("/", auth, async (req, res) => {
   const posts = await prisma.blogPost.findMany();
   res.json({ posts: posts });
+});
+
+router.post("/", auth, async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: req.user,
+    },
+  });
+
+  const validCategories = Object.values(CategoryName);
+
+  if (!validCategories.includes(req.body.category)) {
+    return res.status(400).json({ error: "Invalid category" });
+  }
+
+  let post = {
+    authorId: user.id,
+    title: req.body.title,
+    description: req.body.description,
+    content: req.body.content,
+    category: req.body.category,
+  };
+
+  try {
+    post = await prisma.blogPost.create({ data: post });
+    res.json({ post: post });
+  } catch (error) {
+    console.error("Error creating blog post:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 module.exports = router;
