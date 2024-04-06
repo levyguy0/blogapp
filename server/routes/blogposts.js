1af3e6979a8fca12a0c55cfa4e7f6b15907ad339
levyguy0 <103501989+levyguy0@users.noreply.github.com>
2024-04-06 21:21:41 +0100

    add get

--- server/routes/blogposts.js
+++ server/routes/blogposts.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const { PrismaClient } = require("@prisma/client");
 
 const router = express.Router();
+const prisma = new PrismaClient();
 
-router.get("/", (req, res) => {
-  res.send("hello world");
+router.get("/", async (req, res) => {
+  const posts = await prisma.blogPost.findMany();
+  res.json({ posts: posts });
 });
 
 module.exports = router;
