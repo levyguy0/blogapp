f1c6379278dbb11f68a4eefc472c3ab29d1d8864
Guy Levy <103501989+levyguy0@users.noreply.github.com>
2024-04-06 14:36:43 +0100

    add blogs.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const cookie = require("cookie-parser");
 const users = require("./routes/users");
+const blogposts = require("./routes/blogposts");
 
 const app = express();
 const PORT = 8080;
@@ -24,6 +25,7 @@ app.use(
 app.use(express.json());
 
 app.use("/users", users);
+app.use("/posts", blogposts);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
