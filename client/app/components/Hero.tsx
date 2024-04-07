f3697f7cf428bf427505b5a90524aa27662c65f0
Guy Levy <103501989+levyguy0@users.noreply.github.com>
2024-04-07 17:47:46 +0100

    modify hero

--- client/app/components/Hero.tsx
+++ client/app/components/Hero.tsx
@@ -3,8 +3,10 @@ import React from "react";
 const Hero = () => {
   return (
     <div className="gap-4 flex-col h-screen w-screen flex justify-center items-center">
-      <div className="text-accent text-5xl font-extrabold">Blogify</div>
-      <div className="">When Medium is not enough.</div>
+      <div className="text-secondary text-5xl font-extrabold">Blogify</div>
+      <div className="">
+        When Medium is not <strong>enough</strong>.
+      </div>
     </div>
   );
 };
