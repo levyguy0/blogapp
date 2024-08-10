72a540bacf88197487d710db4d644126a9b18983
Guy Levy <levyguy94@gmail.com>
2024-08-10 16:39:52 +0100

    add key to comments

--- client/app/components/CommentFeed.tsx
+++ client/app/components/CommentFeed.tsx
@@ -10,7 +10,7 @@ const CommentFeed = ({ post }: Props) => {
   return (
     <div>
       {post?.comments.map((c) => (
-        <div className="flex gap-4 flex-col my-2 py-2">
+        <div className="flex gap-4 flex-col my-2 py-2" key={c.id}>
           <div className="flex gap-2">
             <span className="badge badge-secondary">
               <Link href={`/user/${c.authorName}`}>{c.authorName} </Link>
