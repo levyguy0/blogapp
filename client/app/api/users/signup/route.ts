ca6e790e853d09abc1154e50a8a4b8acc9ee9002
Guy Levy <levyguy94@gmail.com>
2024-11-07 22:55:43 +0000

    temp disable signups for demo

--- client/app/api/users/signup/route.ts
+++ client/app/api/users/signup/route.ts
@@ -6,64 +6,74 @@ const bcrypt = require("bcryptjs");
 const prisma = new PrismaClient();
 
 export async function POST(req: NextRequest) {
-  const body = await req.json();
+  const disabled = true;
+  if (!disabled) {
+    const body = await req.json();
 
-  const valid = await SignupUser.safeParseAsync(body);
-  if (valid["success"] == false) {
-    return NextResponse.json(valid["error"], { status: 400 });
-  }
+    const valid = await SignupUser.safeParseAsync(body);
+    if (valid["success"] == false) {
+      return NextResponse.json(valid["error"], { status: 400 });
+    }
 
-  let user = await prisma.user.findUnique({
-    where: {
-      email: body.email,
-    },
-  });
-
-  if (user) {
-    return NextResponse.json(
-      { error: "Email already Registered", path: "email" },
-      { status: 409 }
-    );
-  }
-
-  user = await prisma.user.findUnique({
-    where: {
-      username: body.username,
-    },
-  });
-
-  if (user) {
-    return NextResponse.json(
-      { error: "Username is taken.", path: "username" },
-      { status: 409 }
-    );
-  }
-
-  let new_user = {
-    username: body.username,
-    name: body.name,
-    email: body.email,
-    password: body.password,
-  };
-
-  const salt = await bcrypt.genSalt(10);
-  new_user.password = await bcrypt.hash(new_user.password, salt);
-
-  try {
-    new_user = await prisma.user.create({
-      data: new_user,
-    });
-    return NextResponse.json(
-      {
-        user: {
-          email: new_user.email,
-          name: new_user.name,
-          username: new_user.username,
-        },
+    let user = await prisma.user.findUnique({
+      where: {
+        email: body.email,
       },
-      { status: 200 }
-    );
-  } catch (error) {
-    return NextResponse.json({ error: "Error creating user" }, { status: 500 });
+    });
+
+    if (user) {
+      return NextResponse.json(
+        { error: "Email already Registered", path: "email" },
+        { status: 409 }
+      );
+    }
+
+    user = await prisma.user.findUnique({
+      where: {
+        username: body.username,
+      },
+    });
+
+    if (user) {
+      return NextResponse.json(
+        { error: "Username is taken.", path: "username" },
+        { status: 409 }
+      );
+    }
+
+    let new_user = {
+      username: body.username,
+      name: body.name,
+      email: body.email,
+      password: body.password,
+    };
+
+    const salt = await bcrypt.genSalt(10);
+    new_user.password = await bcrypt.hash(new_user.password, salt);
+
+    try {
+      new_user = await prisma.user.create({
+        data: new_user,
+      });
+      return NextResponse.json(
+        {
+          user: {
+            email: new_user.email,
+            name: new_user.name,
+            username: new_user.username,
+          },
+        },
+        { status: 200 }
+      );
+    } catch (error) {
+      return NextResponse.json(
+        { error: "Error creating user" },
+        { status: 500 }
+      );
+    }
+  } else {
+    return NextResponse.json({
+      message: "Website is in demo mode - please try again later.",
+    });
   }
 }
--- client/app/signup/page.tsx
+++ client/app/signup/page.tsx
@@ -1,16 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import NavBar from "../components/NavBar";
 import SignupCard from "../components/SignupCard";
-import ShownUser from "@/models/ShownUser";
-import axios from "axios";
-import { useRouter } from "next/navigation";
 
 const page = () => {
   return (
     <main>
       <NavBar></NavBar>
       <div className="flex w-screen h-screen justify-center items-center">
-        <SignupCard></SignupCard>
+        {1 != 1 ? (
+          <SignupCard></SignupCard>
+        ) : (
+          <div className="font-extrabold text-2xl flex flex-col items-center justify-center gap-2">
+            <div className="">
+              Website is in demo mode and so signups are temporarily disabled.
+            </div>
+            <div>Please try again later.</div>
+          </div>
+        )}
       </div>
     </main>
   );
